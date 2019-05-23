a38d23a7f1bf8e7865f383410012786cdb651798
mengjingbo <mjbspring@sina.com>
2019-05-23 15:42:07 +0800

    小程序实现列表

---
+++ index/index.js
@@ -0,0 +1,63 @@
+const app = getApp()
+
+Page({
+  
+  data: {
+    language:[
+      "Java",
+      "C",
+      "C++",
+      "Python",
+      ".NET",
+      "C#",
+      "JavaScript", 
+      "SQL",
+      "PHP",
+      "Assembly language",
+      "Objective-C",
+      "Delphi/Object Pascal",
+      "Perl",
+      "MATLAB",
+      "Ruby",
+      "Visual Basic",
+      "Groovy",
+      "Swift",
+      "Go",
+      "PL/SQL",
+      "R",
+      "SAS",
+      "D",
+      "COBOL",
+      "Transact-SQL",
+      "ABAP",
+      "Fortran",
+      "Scratch",
+      "Dart",
+      "Scala",
+      "Prolog",
+      "Lisp",
+      "Lua",
+      "Rust",
+      "Logo",
+      "Ada",
+      "F#",
+      "Apex",
+      "Kotlin",
+      "Scheme",
+      "LabVIEW",
+      "TypeScript",
+      "Julia",
+      "Awk",
+      "Haskell",
+      "Clojure",
+      "Erlang",
+      "ML",
+      "Bash",
+      "RPG"
+    ]
+  },
+  
+  onLoad: function () {
+
+  },
+})
